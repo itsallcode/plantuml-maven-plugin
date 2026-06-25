8d327b55b9b6c5ddd4377520288726cc502133f3
redcatbear <sebastian.baer@exasol.com>
2026-06-25 10:15:20 +0200

    #1: Shorter methods in facade.

--- src/main/java/org/itsallcode/PlantUmlFacade.java
+++ src/main/java/org/itsallcode/PlantUmlFacade.java
@@ -14,7 +14,22 @@ public class PlantUmlFacade {
     private final Class<?> sourceFileReaderClass;
     private final Class<?> fileFormatOptionClass;
     private final Class<?> fileFormatClass;
+    private final Object pngFormat;
+    private final Object svgFormat;
 
+    /**
+     * Create a facade for the PlantUML invocation.
+     * <p>
+     * This class hides all details of the loading and invoking the PlantUML classes. That allows defining the
+     * dependency in the using project's POM file, instead of creating a hard compile time dependency. As long as the
+     * PlantUML API stays compatible, this facade abstracts the implementation details of calling PlantUML.
+     * </p>
+     * <p>
+     * The facade caches loaded classes and objects that can be reused to safe resources and speed-up batch rendering.
+     * </p>
+     *
+     * @param log maven Mojo log
+     */
     public PlantUmlFacade(final Log log) {
         this.loader = getClassLoader();
         this.log = log;
@@ -22,6 +37,8 @@ public class PlantUmlFacade {
             this.sourceFileReaderClass = loadPlantUmlClass("net.sourceforge.plantuml.SourceFileReader");
             this.fileFormatOptionClass = loadPlantUmlClass("net.sourceforge.plantuml.FileFormatOption");
             this.fileFormatClass = loadPlantUmlClass("net.sourceforge.plantuml.FileFormat");
+            this.pngFormat = getPlantUmlFileFormat(ImageType.PNG);
+            this.svgFormat = getPlantUmlFileFormat(ImageType.SVG);
         } catch (final ClassNotFoundException exception) {
             throw new RuntimeException("Unable to load PlantUML classes", exception);
         }
@@ -49,22 +66,15 @@ public class PlantUmlFacade {
         return Class.forName(className, true, this.loader);
     }
 
-    // [impl->dsn~render-png~1]
-    // [impl->dsn~render-svg~1]
-    public void renderFileToDirectory(final File inputFile, final File outputDirectory, final ImageType imageType) {
-        this.log.info("Rendering '" + inputFile + "' to '" + imageType + "' image.");
+    private Object getPlantUmlFileFormat(final ImageType imageType) {
+        final Object fileFormat = resolveEnumConstant(this.fileFormatClass, imageType.name());
+        final Constructor<?> optionConstructor;
         try {
-            final Object fileFormat = resolveEnumConstant(this.fileFormatClass, imageType.name());
-            final Constructor<?> optionConstructor = fileFormatOptionClass.getConstructor(fileFormatClass);
-            final Object options = optionConstructor.newInstance(fileFormat);
-            final Constructor<?> readerConstructor = this.sourceFileReaderClass.getConstructor(boolean.class, File.class
-                    , File.class, this.fileFormatOptionClass);
-            final Object reader = readerConstructor.newInstance(false, inputFile, outputDirectory, options);
-            final Method getGeneratedImagesMethod = this.sourceFileReaderClass.getMethod("getGeneratedImages");
-            getGeneratedImagesMethod.invoke(reader);
-        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException
-                 | IllegalAccessException exception) {
-            throw new RuntimeException(exception);
+            optionConstructor = fileFormatOptionClass.getConstructor(fileFormatClass);
+            return optionConstructor.newInstance(fileFormat);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException
+                 | InvocationTargetException exception) {
+            throw new RuntimeException("Unable to create PlantUML file format " + imageType, exception);
         }
     }
 
@@ -77,5 +87,29 @@ public class PlantUmlFacade {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Enum constant '" + constantName + "' not found in '"
                         + enumClass.getName() + "'"));
+
+    }
+    // [impl->dsn~render-png~1]
+    // [impl->dsn~render-svg~1]
+    public void renderFileToDirectory(final File inputFile, final File outputDirectory, final ImageType imageType) {
+        this.log.info("Rendering '" + inputFile + "' to '" + imageType + "' image.");
+            final Object options = switch(imageType) {
+                case PNG -> this.pngFormat;
+                case SVG -> this.svgFormat;
+            };
+            generateImages(inputFile, outputDirectory, options);
+    }
+
+    private void generateImages(final File inputFile, final File outputDirectory, final Object options) {
+        try {
+            final Constructor<?> readerConstructor = this.sourceFileReaderClass.getConstructor(boolean.class, File.class
+                    , File.class, this.fileFormatOptionClass);
+            final Object reader = readerConstructor.newInstance(false, inputFile, outputDirectory, options);
+            final Method getGeneratedImagesMethod = this.sourceFileReaderClass.getMethod("getGeneratedImages");
+            getGeneratedImagesMethod.invoke(reader);
+            } catch (InvocationTargetException | NoSuchMethodException | InstantiationException
+                     | IllegalAccessException exception) {
+                throw new RuntimeException("Unable to generate images", exception);
+            }
     }
 }
